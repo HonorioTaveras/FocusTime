c4f8d7789dcb04d9d68f9c5ad767f94a3a7db801
Honorio <htaveras90@gmail.com>
2021-04-02 16:12:32 -0600

    rendering sup ditbag! binging in some state variables

--- App.js
+++ App.js
@@ -1,12 +1,12 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, { useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+  const [focusSubject, setFocusSubject] = useState(null);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>Sup dirtbag!</Text>
     </View>
   );
 }
