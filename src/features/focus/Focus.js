71559e49e8aa036f9225788f042bd8c8d93f2fee
Honorio <htaveras90@gmail.com>
2021-04-02 16:45:58 -0600

    added some styles and brought in text input from react native paper

--- src/features/focus/Focus.js
+++ src/features/focus/Focus.js
@@ -1,16 +1,37 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { TextInput } from 'react-native-paper';
 
-export const Focus = () => (
-  <View style={styles.container}>
-    <Text>What would you like to focus on?</Text>
-  </View>
-);
+export const Focus = () => {
+  const { container, titleContainer, title, inputContainer } = styles;
+
+  return (
+    <View style={container}>
+      <View style={titleContainer}>
+        <Text style={title}>What would you like to focus on?</Text>
+        <View style={inputContainer}>
+          <TextInput />
+        </View>
+      </View>
+    </View>
+  );
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
+  },
+  titleContainer: {
+    flex: 0.5,
+    padding: 16,
     justifyContent: 'center',
+  },
+  title: {
+    color: 'white',
+    fontWeight: 'bold',
+    fontSize: 24,
+  },
+  inputContainer: {
+    paddingTop: 20,
   },
 });
