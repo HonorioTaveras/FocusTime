e117c0e5d2d6f62c23da778e70c9b4d398fb820e
Honorio <htaveras90@gmail.com>
2021-04-07 00:26:24 -0600

    got rounded button complete as of right now, still need to test

--- src/Components/RoundedButton.js
+++ src/Components/RoundedButton.js
@@ -1,9 +1,30 @@
 import React from 'react';
 import { TouchableOpacity, Text, StyleSheet } from 'react-native';
 
-export const RoundedButton = ({ style = {}, textStyle = {}, size = 125, ...props }) => {
+export const RoundedButton = ({
+  style = {},
+  textStyle = {},
+  size = 125,
+  ...props
+}) => {
+  const { title } = props;
   return (
-    <TouchableOpacity />
-    <TouchableOpacity />
-  )
+    <TouchableOpacity style={[styles(size).radius, style]}>
+      <Text style={[styles.text, textStyle]}>{title}</Text>
+    </TouchableOpacity>
+  );
 };
+
+const styles = (style) =>
+  StyleSheet.create({
+    radius: {
+      borderRadius: size / 2,
+      width: size,
+      height: size,
+      alignItems: 'center',
+    },
+    text: {
+      color: '#fff',
+      fontSize: 20,
+    },
+  });
